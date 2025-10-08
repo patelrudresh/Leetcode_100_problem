a43b019fbe1f980b6c40df4f8a1ce397f4f9b891
patelrudresh <rudreshpatel728@gmail.com>
2025-10-08 18:22:03 +0530

    removeduplicate_1ST_PROBLEM

---
+++ Arrays_problem/RemoveDuplicate.java
@@ -0,0 +1,59 @@
+package Arrays_problem;
+
+import java.util.Arrays;
+
+public class RemoveDuplicate {
+ //with thetwo pointer appraoch
+/*	public static int duplicateFind(int arr[]) {
+		if(arr.length==0) {
+			return 0;
+		}
+		int i=0;
+		for(int j=1;j<arr.length;j++) {
+			if(arr[j]!=arr[i]) {
+				i++;
+				arr[i]=arr[j];
+			}
+		}
+		return i+1;
+	}*/
+	
+	// solution with streanms
+	/*public static int duplicateNumber(int arr[]) {
+		int []unique=Arrays.stream(arr).distinct().toArray();
+		for(int i=0;i<unique.length;i++) {
+			arr[i]=unique[i];
+		}
+		return unique.length;
+	}*/
+	
+	//using temp array
+	
+	public static int duplicateNumber(int arr[]) {
+		int []temp=new int[arr.length];
+		int j=0;
+		temp[j++]=arr[0];
+		
+		for(int i=1; i<arr.length;i++) {
+			if(arr[i]!=arr[i-1]) {
+				temp[j++]=arr[i];
+			}
+		}
+		for (int i=0; i<temp.length;i++) {
+			arr[i]=temp[i];
+			
+		}
+		return j;
+	}
+	public static void main(String[] args) {
+		int arr[]= {1,1,2,2,3,4,4};
+
+		int newarr=duplicateNumber(arr);
+		System.out.println("dupilcate number is "+newarr);
+		
+		for(int i=0; i<newarr;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
