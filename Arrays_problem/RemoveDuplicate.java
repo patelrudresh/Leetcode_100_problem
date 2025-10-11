cc85d76a83512277c7db0eff771520a1d8cf2515
patelrudresh <rudreshpatel728@gmail.com>
2025-10-11 22:13:38 +0530

    add removesuplicate or single find

--- Arrays_problem/RemoveDuplicate.java
+++ Arrays_problem/RemoveDuplicate.java
@@ -1,10 +1,11 @@
 package Arrays_problem;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class RemoveDuplicate {
  //with thetwo pointer appraoch
-/*	public static int duplicateFind(int arr[]) {
+public static int duplicateNumber(int arr[]) {
 		if(arr.length==0) {
 			return 0;
 		}
@@ -12,11 +13,13 @@ public class RemoveDuplicate {
 		for(int j=1;j<arr.length;j++) {
 			if(arr[j]!=arr[i]) {
 				i++;
-				arr[i]=arr[j];
+				int temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
 			}
 		}
 		return i+1;
-	}*/
+	}
 	
 	// solution with streanms
 	/*public static int duplicateNumber(int arr[]) {
@@ -29,26 +32,32 @@ public class RemoveDuplicate {
 	
 	//using temp array
 	
-	public static int duplicateNumber(int arr[]) {
-		int []temp=new int[arr.length];
-		int j=0;
-		temp[j++]=arr[0];
-		
-		for(int i=1; i<arr.length;i++) {
-			if(arr[i]!=arr[i-1]) {
-				temp[j++]=arr[i];
-			}
-		}
-		for (int i=0; i<temp.length;i++) {
-			arr[i]=temp[i];
-			
-		}
-		return j;
-	}
+//	public int duplicateNumber(int arr[]) {
+//		int []temp=new int[arr.length];
+//		int j=0;
+//		temp[j++]=arr[0];
+//		
+//		for(int i=1; i<arr.length;i++) {
+//			if(arr[i]!=arr[i-1]) {
+//				temp[j++]=arr[i];
+//			}
+//		}
+//		for (int i=0; i<temp.length;i++) {
+//			arr[i]=temp[i];
+//			
+//		}
+//		return j;
+//	}
 	public static void main(String[] args) {
+		RemoveDuplicate rd=new RemoveDuplicate();
+		Scanner sc=new Scanner(System.in);
 		int arr[]= {1,1,2,2,3,4,4};
-
-		int newarr=duplicateNumber(arr);
+//    int size=sc.nextInt();
+//    int arr[] =new int[size];
+//    for(int num=0;num<size;num++) {
+//    arr[num]=sc.nextInt();
+//    }
+		int newarr=rd.duplicateNumber(arr);
 		System.out.println("dupilcate number is "+newarr);
 		
 		for(int i=0; i<newarr;i++) {
---
+++ Arrays_problem/Single_Find.java
@@ -0,0 +1,5 @@
+package Arrays_problem;
+
+public class Single_Find {
+
+}
